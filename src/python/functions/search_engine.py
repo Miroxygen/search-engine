7c930cba1de0eb25807253a4492c2c54f8764948
Miranda <mh225wi@student.lnu.se>
2023-12-04 11:38:50 +0100

    added start of search function multiword

--- src/python/functions/search_engine.py
+++ src/python/functions/search_engine.py
@@ -5,3 +5,14 @@ class SearchEngine:
 
     def __init__(self):
         self.page_db = PageDataBase()
+
+    def search(self, query):
+        split_query = query.split()
+        word_ids = []
+        for query in split_query:
+            id = self.page_db.get_word_id(query)
+            word_ids.append(id)
+        matching_pages = []
+        for page in self.page_db.pages:
+            if all(ids in page.words for ids in word_ids):
+                matching_pages.append(page)
